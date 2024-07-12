488449cc9f1bf38f554884a922cbbe7132065047
Akhil Aroly <125636276+akhil967-hub@users.noreply.github.com>
2024-07-12 12:06:07 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
