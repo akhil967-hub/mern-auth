962ade65575e2f7039871cdb4f243acba10272c4
Akhil Aroly <125636276+akhil967-hub@users.noreply.github.com>
2024-07-16 19:45:11 +0530

    complete image  upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
             <li>Home</li>
           </Link>
           <Link to="/about">
-            <li>About</li>
+            <li>About</li> 
           </Link>
           <Link to="/profile">
             {currentUser ? (
